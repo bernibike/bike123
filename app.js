cf02e9c5b01efea3bfadcbf010a2c173d7af9872
bernibike <165203341+bernibike@users.noreply.github.com>
2024-03-27 19:42:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+        // Iniciar Quagga para escanear el código de barras
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                target: document.querySelector('#video')    // Pasa el elemento video a Quagga
+            },
+            decoder: {
+                readers: ["code_128_reader"]  // Ajusta según el tipo de código de barras
+            }
+        }, function(err) {
+            if (err) {
+                console.log(err);
+                return
+            }
+            Quagga.start();
+        });
+
+        Quagga.onDetected(function(result) {
+            var code = result.codeResult.code;
+            enviarCodigoABackend(code);
+            Quagga.stop(); // Detener Quagga después de un escaneo exitoso
+        });
+    }
+});
+
+function enviarCodigoABackend(codigo) {
+    var url = 'URL_DE_TU_WEB_APP';  // Reemplaza con la URL de tu Google Apps Script web app
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors', // Esto puede variar según tu configuración
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({codigo: codigo})
+    })
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
