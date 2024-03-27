4ebb36b5a69a8c716365bd95772e7d11e1fb65cd
bernibike <165203341+bernibike@users.noreply.github.com>
2024-03-27 20:27:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 document.getElementById('startButton').addEventListener('click', function() {
+    console.log('Botón presionado. Solicitando acceso a la cámara...');
     if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-        // Iniciar Quagga para escanear el código de barras
+        console.log('mediaDevices disponible');
         Quagga.init({
             inputStream: {
                 name: "Live",
                 type: "LiveStream",
-                target: document.querySelector('#video'),    // Pasa el elemento video a Quagga
+                target: document.querySelector('#video'), // Pasa el elemento video a Quagga
                 constraints: {
                     facingMode: "environment" // Intenta usar la cámara trasera en dispositivos móviles
                 },
             },
             decoder: {
-                readers: ["code_128_reader"]  // Ajusta según el tipo de código de barras
+                readers: ["code_128_reader"] // Ajusta según el tipo de código de barras
             }
         }, function(err) {
             if (err) {
-                console.log(err);
-                return
+                console.error('Error al iniciar Quagga:', err);
+                return;
             }
+            console.log('Quagga inicializado correctamente');
             Quagga.start();
         });
 
         Quagga.onDetected(function(result) {
-            var code = result.codeResult.code;
-            enviarCodigoABackend(code);
+            console.log('Código de barras detectado:', result.codeResult.code);
+            enviarCodigoABackend(result.codeResult.code);
             Quagga.stop(); // Detener Quagga después de un escaneo exitoso
         });
+    } else {
+        console.error('mediaDevices.getUserMedia no está disponible');
     }
 });
-
 function enviarCodigoABackend(codigo) {
     var url = 'URL_DE_TU_WEB_APP';  // Reemplaza con la URL de tu Google Apps Script web app
     fetch(url, {
